e7d5c65f5cc8c21406012547fcddfff2e0aafe24
Toni Mitrevski <toni@stativa.com.mk>
2019-03-03 22:33:01 +0100

    fix reoute

---
+++ build/precache-manifest.327c5cc0057c6cc4a637478cd959003c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "5ad5f6110a4496df9917",
+    "url": "./static/js/main.5ad5f611.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "./static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5ad5f6110a4496df9917",
+    "url": "./static/css/main.272002b0.chunk.css"
+  },
+  {
+    "revision": "648300a5e61525aada42c71464232ff0",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.444d7b60b21eb5c53b1e092a4237c96d.js"
+  "./precache-manifest.327c5cc0057c6cc4a637478cd959003c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
